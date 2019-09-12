153b8376d21324b6a323fdf250f4a47e21413283
thto <thiagohtoliva@gmail.com>
2019-09-12 09:31:12 +0200

    structure updates

---
+++ 16.ScoreTracker Project/scoreTracker.js
@@ -0,0 +1,10 @@
+var buttonOne = document.querySelector("#buttonOne");
+var buttonTwo = document.querySelector("#buttonTwo");
+
+var scoreOne = document.querySelector("#scoreOne");
+var scoretwo = document.querySelector("#scoretwo");
+
+
+buttonOne.addEventListener("click", function(){
+
+});
