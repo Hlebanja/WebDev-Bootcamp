be268d6cb3ce1979dee9c2b33c9f34c47dbb495a
thto <thiagohtoliva@gmail.com>
2019-10-01 00:24:08 +0200

    finished simple API to interact with OMDB API

--- 27.MovieAPI/app.js
+++ 27.MovieAPI/app.js
@@ -4,8 +4,13 @@ var request = require("request");
 
 app.set("view engine", "ejs");
 
+app.get("/", function(req, res) {
+    res.render("search");
+});
+
 app.get("/results", function(req, res) {
-    request("http://www.omdbapi.com/?s=iowa&apikey=thewdb", function(error, response, body) {
+    var queryResult = req.query.search;
+    request("http://www.omdbapi.com/?s=" + queryResult + "&apikey=thewdb", function(error, response, body) {
         if(!error && response.statusCode == 200) {
            var data = JSON.parse(body); 
             // res.send(data);
