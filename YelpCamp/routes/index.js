459f366871ee6986e3b3b7a323fbfa1fe09cee09
thto <thiagohtoliva@gmail.com>
2020-01-17 13:58:43 +0100

    add start script for heroku.

--- YelpCamp/routes/index.js
+++ YelpCamp/routes/index.js
@@ -35,7 +35,7 @@ router.get("/login", function (req, res) {
 router.post("/login", passport.authenticate("local", {
     successRedirect: "/dogs",
     failureRedirect: "/login"
-}), function (req, res) { }
+}), function (req, res) {}
 );
 
 router.get("/logout", function (req, res) {
